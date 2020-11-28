d1e6e92fc1a9e41a7a1344e8e68afe42fa06c54f
alshaV888 <alfi15-3594@diu.edu.bd>
2020-11-28 11:24:31 +0600

    sol.cpp

---
+++ sol.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+void solve(){
+
+}
+
+int main(){
+	ios::syn_with_stdio(0);
+	cin.tie(0);
+
+	int T;
+	cin >> T;
+	while(T--){
+		solve();
+	}
+
+	return 0;
+}
